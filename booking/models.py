e8e620bc8a00d1aba8e4bc827d337a1f15e55df0
Ebba Grönvall <ebbagronvall@hotmail.com>
2024-08-02 14:30:37 +0000

    Booking model created

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Booking(models.Model):
+    customer = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="customer_booking")
+    appointment = models.DateTimeField()
+    created_on = models.DateTimeField(auto_now_add=True)
