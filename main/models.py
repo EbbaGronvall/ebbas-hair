cef2acf92d0bab740948a771a2fef23fb3652fe0
Ebba Grönvall <ebbagronvall@hotmail.com>
2024-08-03 15:30:43 +0000

    Added Stylist model and made migrations to server

--- main/models.py
+++ main/models.py
@@ -3,8 +3,18 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
+class Stylist(models.Model):
+    stylist_name = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField()
+    specialties = models.CharField(max_length=255)
+    price = models.IntegerField()
+
+
 class Booking(models.Model):
     customer = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="customer_booking")
+    stylist = models.ForeignKey(
+        Stylist, on_delete=models.CASCADE, related_name="stylist_booking", null=True, blank=True)
     appointment = models.DateTimeField()
     created_on = models.DateTimeField(auto_now_add=True)
