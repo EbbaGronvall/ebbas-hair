1bbad77ecb2aa8a5f100c3ceb688db6f083afff2
Ebba Grönvall <ebbagronvall@hotmail.com>
2024-08-04 19:35:46 +0000

    Stylist model moved to about app

--- about/models.py
+++ about/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Stylist(models.Model):
+    stylist_name = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField()
+    specialties = models.CharField(max_length=255)
+    price = models.IntegerField()
