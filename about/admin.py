6e0a413a0c53718b45bb7c1bb6fda722144c27f7
Ebba Grönvall <ebbagronvall@hotmail.com>
2024-08-05 15:22:45 +0000

    Added summernote so that the admin can make the stylists bios prettier

--- about/admin.py
+++ about/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Stylist
+from django_summernote.admin import SummernoteModelAdmin
 
+@admin.register(Stylist)
+class AboutAdmin(SummernoteModelAdmin):
+
+    list_display = ('stylist_name', 'speciality', 'price')
+    search_fields = ['stylist_name']
+    list_filter = ('speciality',)
+    summernote_fields = ('bio',)
 # Register your models here.
 
-admin.site.register(Stylist)
