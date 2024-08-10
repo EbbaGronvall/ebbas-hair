3aa008e311423347409ef4c0965327666f81199c
Ebba Grönvall <ebbagronvall@hotmail.com>
2024-08-10 16:06:29 +0000

    Made the day field in the booking form to  a datefield and added placeholdertext to textfiled and made sure a booking cant be made without a stylist chosen

--- booking/forms.py
+++ booking/forms.py
@@ -6,13 +6,26 @@ class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ['stylist', 'day', 'time', 'comment']
+        widgets = {
+            'day': forms.DateInput(attrs={'type': 'date'}),
+            'comment': forms.Textarea(attrs={'placeholder': 'Is there anything we need to know about your hair beforehand?'})
+        }
 
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.fields['time'].choices = TIME_CHOICES
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['time'].choices = TIME_CHOICES
 
-        def clean_day(self):
-            day = self.cleaned_data.get('day')
-            if day < datetime.now().date():
-                raise forms.ValidationError('You cannot book an appointment in the past.')
-            return day
+    def clean(self):
+        cleaned_data = super().clean()
+        stylist = cleaned_data.get("stylist")
+
+        if not stylist:
+            raise forms.ValidationError('You must select a stylist.')
+
+        return cleaned_data
+
+    def clean_day(self):
+        day = self.cleaned_data.get('day')
+        if day < datetime.now().date():
+            raise forms.ValidationError('You cannot book an appointment in the past.')
+        return day
